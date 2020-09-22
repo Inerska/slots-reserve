b481d3c0ad0800f8cd488d968a909a1b312fe233
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-09-22 23:00:58 +0200

    basic http server with mysql handling

--- src/database/server.go
+++ src/database/server.go
@@ -1,3 +1,14 @@
 package database
 
+import (
+	"database/sql"
+	"fmt"
+)
 
+func EtablishSQLConnexion(host string, userid string, password string, driver string, port int) *sql.DB{
+	db, err := sql.Open(driver, fmt.Sprintf("%s:%s@tcp(%s:%d)/", userid, password, host, port))
+	if err != nil{
+		panic(err)
+	}
+	return db
+}
--- src/main.go
+++ src/main.go
@@ -1,6 +1,13 @@
 package main
 
-//import "./database"
+import (
+	"./database"
+	"github.com/prometheus/common/log"
+	"net/http"
+)
 
-
-
+func main() {
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	db := database.EtablishSQLConnexion("localhost", "root", "root", "mysql", 8080)
+	_ = db.Close()
+}
