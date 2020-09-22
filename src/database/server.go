ceab6041ea89eaf7f3f25762398185861a0288c6
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-09-22 23:12:18 +0200

    Loading static through back

--- src/database/server.go
+++ src/database/server.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 )
 
-func EtablishSQLConnexion(host string, userid string, password string, driver string, port int) *sql.DB{
+func EtablishSQLConnexion(host string, userid string, password string, driver string, port int) *sql.DB {
 	db, err := sql.Open(driver, fmt.Sprintf("%s:%s@tcp(%s:%d)/", userid, password, host, port))
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	return db
--- src/main.go
+++ src/main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./static"))))
+
 	db := database.EtablishSQLConnexion("localhost", "root", "root", "mysql", 8080)
 	_ = db.Close()
 }
