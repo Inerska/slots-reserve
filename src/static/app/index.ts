9f8464f80945b2a7b7c57eeea9a050a8ff97a5ec
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-09-23 17:43:41 +0200

    removing debug

--- src/static/app/index.ts
+++ src/static/app/index.ts
@@ -1,9 +1,8 @@
 // On document fully loaded
 document.addEventListener("DOMContentLoaded", ()=>{
-    console.log("Hey 1");
     const btn_housekeeping = document.getElementById("housekeeping-enter");
 
     btn_housekeeping.addEventListener("click", ()=>{
-       location.href = "/housekeeping";
+        location.href = "/housekeeping";
     });
 });
