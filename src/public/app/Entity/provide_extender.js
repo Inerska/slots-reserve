fe4ab7e7ff8b140e7655a6b50b574cb059ff25e1
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-24 13:19:12 +0200

    js minor edits

--- src/public/app/Entity/provide_extender.js
+++
@@ -1,9 +0,0 @@
-export function provide_click(event) {
-
-    const modal = document.querySelector('.modal');
-
-    event.preventDefault();
-    modal.classList.toggle('opacity-0');
-    modal.classList.toggle('pointer-events-none');
-    $('body').classList.toggle('modal-active');
-}
---
+++ src/public/index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const reserv_btn = document.querySelector(".modal-open");
+    const modal = document.querySelector(".modal");
+
+    // Event dispatcher
+    reserv_btn.addEventListener("click", evt => toggleModal(evt));
+
+    // Calendar instance
+    $(".calendario").flatpickr({
+        enableTime: true,
+        minTime: "08:00",
+        maxTime: "18:00",
+        dateFormat: "Y-m-d H:i"
+    });
+});
+
+function toggleModal(event) {
+    const modal = document.querySelector('.modal')
+
+    event.preventDefault();
+    modal.classList.toggle('opacity-0')
+    modal.classList.toggle('pointer-events-none')
+    $('body').classList.toggle('modal-active')
+}
