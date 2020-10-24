d6c8f542f3b7c0abafd63e3b6285d576d3a4ef45
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-24 17:31:23 +0200

    Add modal context button

--- src/public/index.js
+++ src/public/index.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const reserv_btn = document.querySelector(".modal-open");
     const modal = document.querySelector(".modal");
+    const close_modal = document.querySelectorAll('.modal-close');
 
     // Event dispatcher
-    reserv_btn.addEventListener("click", evt => toggleModal(evt));
+    reserv_btn.addEventListener('click', evt => toggleModal(evt));
+
+    for (let i = 0; i < close_modal.length; i++) {
+        close_modal[i].addEventListener('click', evt => toggleModal(evt));
+    }
+
 
     // Calendar instance
     $(".calendario").flatpickr({
@@ -16,10 +22,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Toggle the modal passing the event instance
 function toggleModal(event) {
-    const modal = document.querySelector('.modal')
+    const modal = document.querySelector('.modal');
+    const body = document.querySelector('body');
 
     event.preventDefault();
-    modal.classList.toggle('opacity-0')
-    modal.classList.toggle('pointer-events-none')
-    $('body').classList.toggle('modal-active')
+    modal.classList.toggle('opacity-0');
+    modal.classList.toggle('pointer-events-none');
+    body.classList.toggle('modal-active');
 }
