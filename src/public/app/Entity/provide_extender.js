fd25e97bde245755efde32ebb80af7db9ae0c911
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-24 13:07:13 +0200

    Function effect assignement

--- src/public/app/Entity/provide_extender.js
+++ src/public/app/Entity/provide_extender.js
@@ -1,4 +1,9 @@
 export function provide_click(event) {
-    event.preventDefault();
 
+    const modal = document.querySelector('.modal');
+
+    event.preventDefault();
+    modal.classList.toggle('opacity-0');
+    modal.classList.toggle('pointer-events-none');
+    $('body').classList.toggle('modal-active');
 }
