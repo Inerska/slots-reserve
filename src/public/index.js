d632063fa45e9174700566e8f3f92bc3eddcde6f
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-29 13:07:35 +0100

    Add checkbox link ajax

--- src/public/index.js
+++ src/public/index.js
@@ -1,16 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // const reserv_btn = document.querySelector(".modal-open");
-    // const modal = document.querySelector(".modal");
-    // const close_modal = document.querySelectorAll('.modal-close');
-    // const post_pro = document.querySelector('.post');
-    //
-    // // Event dispatcher
-    // reserv_btn.addEventListener('click', evt => toggleModal(evt));
-    // post_pro.addEventListener('click', send_ajax);
-    //
-    // for (let i = 0; i < close_modal.length; i++) {
-    //     close_modal[i].addEventListener('click', evt => toggleModal(evt));
-    // }
+    const reserv_btn = document.querySelector("#form_validate");
+
+    reserv_btn.addEventListener('click', send_ajax);
+
 
     // Calendar instance
     $(".calendario").flatpickr({
@@ -19,25 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
         maxTime: "18:00",
         dateFormat: "Y-m-d H:i"
     });
-
-    $('.calendario').on('click', () => {
-        $('.calendario').val('');
-    })
 });
-//
-// // Toggle the modal passing the event instance
-// function toggleModal(event) {
-//     if (areInputsNull()) {
-//         const modal = document.querySelector('.modal');
-//         const body = document.querySelector('body');
-//
-//         event.preventDefault();
-//         modal.classList.toggle('opacity-0');
-//         modal.classList.toggle('pointer-events-none');
-//         body.classList.toggle('modal-active');
-//     } else alert("Veuillez remplir tous les champs disponibles.");
-// }
-//
+
 //
 // // Are inputs null
 // function areInputsNull() {
@@ -50,19 +25,22 @@ document.addEventListener("DOMContentLoaded", () => {
 //     return isnull;
 // }
 //
-// function send_ajax() {
-//     let request = $.post({
-//         type: "POST",
-//         contentType: "json",
-//         url: "../model/post-reservation.php",
-//         data: {
-//             prename: $('#firstname').val(),
-//             name: $('#lastname').val(),
-//             mail: $('#mail').val(),
-//             phone: $('#phone-number').val()
-//         },
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
-// }
+function send_ajax() {
+    $.post({
+        type: "POST",
+        contentType: "json",
+        url: "../model/post-reservation.php",
+        data: {
+            prename: $('#firstname').val(),
+            name: $('#lastname').val(),
+            mail: $('#mail').val(),
+            phone: $('#phone-number').val(),
+            activity: $('#activity').text(),
+            date: $('.calendario').val(),
+            bapteme: $('#bapteme').val()
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
