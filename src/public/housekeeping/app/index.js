5153ff491148fff4d771e858564bb0a86249ac78
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-30 18:42:56 +0100

    [stable] system calendar

--- src/public/housekeeping/app/index.js
+++ src/public/housekeeping/app/index.js
@@ -1,24 +1,104 @@
-const events = [{
-    "title": "Something",
-    "start": "2020-10-30 08:40:00",
-    "end": "2020-10-30 08:40:00",
-    "backgroundColor": "#1b1b1b"
-}]
+// let events = [];
+//
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//     const calendar_element = document.getElementById('calendar');
+//
+//     const options = {
+//         locale: 'fr',
+//         initialView: 'timeGridWeek',
+//         buttonText: {
+//             today: "Aujourd\'hui"
+//         },
+//         header: {
+//             center: 'addEventButton'
+//         },
+//         customButtons: {
+//             addEventButton: {
+//                 text: 'Ajouter une permanence',
+//                 click: () => {
+//                     let author = prompt('A quel nom ?');
+//                     let datestr = prompt('A quel moment ? (YYYY-MM-DD HH:ii) ex: 2020-30-10 10:30');
+//                     let date = moment(datestr);
+//                     if (date.isValid()) {
+//                         calendar_event.renderEvent({
+//                             "title": "test",
+//                             "start": "2020-10-30 08:40:00",
+//                             "end": "2020-10-30 08:45:00",
+//                             "backgroundColor": "#1b1b1b"
+//                         });
+//                     }else{
+//                         prompt("Format de date invalide");
+//                     }
+//                 }
+//             }
+//         },
+//         events: events,
+//         nowIndicator: true
+//     }
+//     let calendar_event = $('#calendar').fullCalendar(options);
+//
+//     calendar_event.render();
+// });
 
+// function NewPermanence() {
+//     const today = Date.now();
+//
+//     events.push({
+//         "title": "Permanence par Joffrey",
+//         "start": `${today}`,
+//         "end": `${today}`,
+//         "backgroundColor": "#FF0000"
+//     });
+//     $('#calendar').fullCalendar('refetchEvents');
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    let calendar_element = document.getElementById('calendar');
+// let events = [];
+//
+// $(function () {
+//     const options = {
+//         locale: 'fr',
+//         initialView: 'timeGridWeek',
+//         buttonText: {
+//             today: "Aujourd\'hui"
+//         },
+//         header: {
+//             center: 'addEventButton'
+//         },
+//         customButtons: {
+//             addEventButton: {
+//                 text: 'Ajouter une permanence',
+//                 click: () => {
+//                     let author = prompt('A quel nom ?');
+//                     let datestr = prompt('A quel moment ? (YYYY-MM-DD HH:ii) ex: 2020-30-10 10:30');
+//                     let date = moment(datestr);
+//                     if (date.isValid()) {
+//                         calendar_event.renderEvent({
+//                             "title": "test",
+//                             "start": "2020-10-30 08:40:00",
+//                             "end": "2020-10-30 08:45:00",
+//                             "backgroundColor": "#1b1b1b"
+//                         });
+//                         console.log("hey");
+//                     }else{
+//                         alert("Format de date invalide");
+//                     }
+//                 }
+//             }
+//         },
+//         events: events,
+//         nowIndicator: true
+//     }
+//     $('#calendar').fullCalendar();
+// });
 
-    const options = {
-        locale: 'fr',
+document.addEventListener('DOMContentLoaded', () => {
+    let calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
         initialView: 'timeGridWeek',
-        buttonText: {
-            today: "Aujourd\'hui"
-        },
-        events: events,
-        nowIndicator: true
-    }
-    let calendar_event = new FullCalendar.Calendar(calendar_element, options);
-
-    calendar_event.render();
+        locale: 'fr',
+        header: {
+            center: 'addEventButton'
+        }
+    });
+    calendar.render();
 });
