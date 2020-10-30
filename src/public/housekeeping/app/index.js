d20d0ae194896c2e6decf89a54e6540cfea2ae18
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-30 16:43:13 +0100

    Test full calendar event

--- src/public/housekeeping/app/index.js
+++ src/public/housekeeping/app/index.js
@@ -1,11 +1,22 @@
+const events = [{
+    "title": "Something",
+    "start": "2020-10-30 08:40:00",
+    "end": "2020-10-30 08:40:00",
+    "backgroundColor": "#1b1b1b"
+}]
+
+
 document.addEventListener("DOMContentLoaded", () => {
     let calendar_element = document.getElementById('calendar');
+
     const options = {
         locale: 'fr',
         initialView: 'timeGridWeek',
         buttonText: {
             today: "Aujourd\'hui"
-        }
+        },
+        events: events,
+        nowIndicator: true
     }
     let calendar_event = new FullCalendar.Calendar(calendar_element, options);
 
