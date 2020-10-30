4ccd47e45719ea93d6d475592bcf3de241977ebd
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-10-30 19:06:48 +0100

    stable back calendar

--- src/public/housekeeping/app/index.js
+++ src/public/housekeeping/app/index.js
@@ -1,104 +1,38 @@
-// let events = [];
-//
-//
-// document.addEventListener("DOMContentLoaded", () => {
-//     const calendar_element = document.getElementById('calendar');
-//
-//     const options = {
-//         locale: 'fr',
-//         initialView: 'timeGridWeek',
-//         buttonText: {
-//             today: "Aujourd\'hui"
-//         },
-//         header: {
-//             center: 'addEventButton'
-//         },
-//         customButtons: {
-//             addEventButton: {
-//                 text: 'Ajouter une permanence',
-//                 click: () => {
-//                     let author = prompt('A quel nom ?');
-//                     let datestr = prompt('A quel moment ? (YYYY-MM-DD HH:ii) ex: 2020-30-10 10:30');
-//                     let date = moment(datestr);
-//                     if (date.isValid()) {
-//                         calendar_event.renderEvent({
-//                             "title": "test",
-//                             "start": "2020-10-30 08:40:00",
-//                             "end": "2020-10-30 08:45:00",
-//                             "backgroundColor": "#1b1b1b"
-//                         });
-//                     }else{
-//                         prompt("Format de date invalide");
-//                     }
-//                 }
-//             }
-//         },
-//         events: events,
-//         nowIndicator: true
-//     }
-//     let calendar_event = $('#calendar').fullCalendar(options);
-//
-//     calendar_event.render();
-// });
+let calendar;
+let events = [];
 
-// function NewPermanence() {
-//     const today = Date.now();
-//
-//     events.push({
-//         "title": "Permanence par Joffrey",
-//         "start": `${today}`,
-//         "end": `${today}`,
-//         "backgroundColor": "#FF0000"
-//     });
-//     $('#calendar').fullCalendar('refetchEvents');
-// }
-
-// let events = [];
-//
-// $(function () {
-//     const options = {
-//         locale: 'fr',
-//         initialView: 'timeGridWeek',
-//         buttonText: {
-//             today: "Aujourd\'hui"
-//         },
-//         header: {
-//             center: 'addEventButton'
-//         },
-//         customButtons: {
-//             addEventButton: {
-//                 text: 'Ajouter une permanence',
-//                 click: () => {
-//                     let author = prompt('A quel nom ?');
-//                     let datestr = prompt('A quel moment ? (YYYY-MM-DD HH:ii) ex: 2020-30-10 10:30');
-//                     let date = moment(datestr);
-//                     if (date.isValid()) {
-//                         calendar_event.renderEvent({
-//                             "title": "test",
-//                             "start": "2020-10-30 08:40:00",
-//                             "end": "2020-10-30 08:45:00",
-//                             "backgroundColor": "#1b1b1b"
-//                         });
-//                         console.log("hey");
-//                     }else{
-//                         alert("Format de date invalide");
-//                     }
-//                 }
-//             }
-//         },
-//         events: events,
-//         nowIndicator: true
-//     }
-//     $('#calendar').fullCalendar();
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
-    let calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
+    const calendar_element = document.getElementById('calendar');
+    const perm_button = document.getElementById('perm_val');
+
+    perm_button.addEventListener('click', NewPermanence);
+
+    calendar = new FullCalendar.Calendar(calendar_element, {
         initialView: 'timeGridWeek',
         locale: 'fr',
-        header: {
-            center: 'addEventButton'
-        }
+        events: events
     });
     calendar.render();
 });
+
+
+function NewPermanence() {
+    const today = Date.now();
+    const calendar_element = document.getElementById('calendar');
+
+    events.push({
+        "title": "Permanence par Jofrey",
+        "start": "2020-10-30 08:40:00",
+        "end": "2020-10-30 10:45:00",
+        "backgroundColor": "#1b1b1b"
+    })
+    calendar.destroy();
+    calendar = new FullCalendar.Calendar(calendar_element, {
+        initialView: 'timeGridWeek',
+        locale: 'fr',
+        events: events,
+        nowIndicator: true
+    });
+    calendar.render();
+}
